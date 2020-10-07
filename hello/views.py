a010e62ee4aa6b6706745e985c9b0f31dbc23473
fors0 <jensfors0@gmail.com>
2020-10-07 13:15:44 +0200

    work plox

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("GIDER DU IKKE GODT AT VIRKE!")
+    return HttpResponse("Please work babygirl!")
